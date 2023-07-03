5fa57854872cf6c7e6703a3586afbe2569438219
Dmitriy.Tokarev <kerlon1301@gmail.com>
2023-07-03 23:20:50 +0200

    Add footer

--- HomeWorks/Front end/Project_Dark_Light/scripts/index.js
+++ HomeWorks/Front end/Project_Dark_Light/scripts/index.js
@@ -1,13 +1,22 @@
-// Загрузка nav.html
-fetch('components/nav.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
+const createIndex = async ()=> {
+    await fetch('components/nav.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
 
-  fetch('components/home.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('home').innerHTML = data;
-  });
+    await fetch('components/home.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('home').innerHTML = data;
+    });
+
+    await fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", createIndex);
 
