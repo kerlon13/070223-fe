72d2cdb8e151ef271dff000376d21621fa14e9de
Dmitriy.Tokarev <kerlon1301@gmail.com>
2023-09-10 14:29:21 +0200

    add edit personal info

---
+++ HomeWorks/Front end/React/personal_area/src/components/Input/index.jsx
@@ -0,0 +1,50 @@
+import "./index.css";
+import icon from "../../assets/icons/icon_edit.png";
+import saveIcon from "../../assets/icons/icon-save.png"
+import { useState } from "react";
+
+function Input({value, title}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [inputValue, setInputValue] = useState(value);
+    const [newValue, setNewValue] = useState(value);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSaveClick = () => {
+        console.log('Сохранено значение:', inputValue);
+        setNewValue(inputValue);
+        setIsEditing(false);
+    };
+    console.log(title);
+    return(
+        
+        <div >
+            {isEditing==false ? (
+                <div className="input-container">
+                    <div>
+                        <label>{title}:</label>
+                        <p>{newValue}</p>
+                    </div>
+                    {title === "id" || title ==="username" || title ==="email" ? null : 
+                    <button onClick={handleEditClick}><img src={icon} className="icon"/></button>}
+                </div>
+            ) : (
+                <div className="input-container">
+                    <div>
+                        <label>{title}</label>
+                        <input type="text" value={inputValue} onChange={handleInputChange} />
+                    </div>
+                    <button on onClick={handleSaveClick}><img src={saveIcon} className="icon"/></button>
+                </div>
+            )}    
+        </div>
+    )
+}
+
+export default Input;
--- HomeWorks/Front end/React/personal_area/src/pages/PersonalInfo/index.jsx
+++ HomeWorks/Front end/React/personal_area/src/pages/PersonalInfo/index.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
-import './index.css'
-function PersonalInfo({user}) {
-    
-    return (
-        <div className="user-profile">
-          <h2>User Profile</h2>
-          <div className="user-info">
-            <p><strong>Name:</strong> {user.name}</p>
-            <p><strong>Username:</strong> {user.username}</p>
-            <p><strong>Email:</strong> {user.email}</p>
-            <p><strong>Phone:</strong> {user.phone}</p>
-            <p><strong>Website:</strong> {user.website}</p>
-          </div>
-          <div className="user-address">
-            <h3>Address</h3>
-            <p><strong>Street:</strong> {user.address.street}</p>
-            <p><strong>Suite:</strong> {user.address.suite}</p>
-            <p><strong>City:</strong> {user.address.city}</p>
-            <p><strong>Zipcode:</strong> {user.address.zipcode}</p>
-          </div>
-          <div className="user-company">
-            <h3>Company</h3>
-            <p><strong>Name:</strong> {user.company.name}</p>
-            <p><strong>Catch Phrase:</strong> {user.company.catchPhrase}</p>
-            <p><strong>Business:</strong> {user.company.bs}</p>
-          </div>
-        </div>
-      );
+import Input from '../../components/Input';
+import React from "react";
 
+function PersonalInfo({ user }) {
+  
+  const renderInputs = (obj, parentKey = "") => {
+    return Object.entries(obj).map(([key, value]) => {
+      const currentKey = parentKey ? `${parentKey} ${key}` : key;
+      if (typeof value === "object") {
+        return renderInputs(value, currentKey);
+      } else {
+        return <Input key={currentKey} title={currentKey} value={value} />;
+      }
+    });
+  };
+
+  return (
+    <div className="user-profile">
+      {renderInputs(user)}
+    </div>
+  );
 }
 
 export default PersonalInfo;
